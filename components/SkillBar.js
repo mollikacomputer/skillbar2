50adff72d0d87eba0dcf5b5e63cb199668f5db5f
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-16 17:24:43 +0600

    skill bar added done

--- components/SkillBar.js
+++ components/SkillBar.js
@@ -3,8 +3,9 @@ const SkillBar = () => {
   return (
     <div className="styles.skillContainer">
       <div className={styles.titleText}>Skill Lavel</div>
-      {/* html skill */}
+      
       <div className={styles.skill}>
+        {/* html skill */}
         <div className={styles.skillBox}>
           <span className={styles.title}>HTML</span>
           <div className={styles.skillBar}>
@@ -13,6 +14,25 @@ const SkillBar = () => {
             </span>
           </div>
         </div>
+        {/* CSS skill */}
+        <div className={styles.skillBox}>
+          <span className={styles.title}>CSS</span>
+          <div className={styles.skillBar}>
+            <span className={`${styles.skillPer} ${styles.css}`}>
+              <span className={styles.tooltip}>80%</span>
+            </span>
+          </div>
+        </div>
+        {/* Javascript skill */}
+        <div className={styles.skillBox}>
+          <span className={styles.title}>Javascript</span>
+          <div className={styles.skillBar}>
+            <span className={`${styles.skillPer} ${styles.javascript}`}>
+              <span className={styles.tooltip}>70%</span>
+            </span>
+          </div>
+        </div>
+
       </div>
     </div>
   );
