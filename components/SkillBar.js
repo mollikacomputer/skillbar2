67fabf76cd89b148a22104a8a2af50a9ef950cc8
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-16 11:52:35 +0600

    html skill bar ui allmost done

--- components/SkillBar.js
+++ components/SkillBar.js
@@ -1,20 +1,20 @@
-import styles from '@/styles/skillbar.module.css'
+import styles from '@/styles/skillbar.module.css';
 const SkillBar = () => {
   return (
-        <div className="styles.skillContainer">
-            <div className="styles.titleText">Skill Lavel</div>
-            {/* html skill */}
-            <div className="styles.skill">
-                <div className="styles.skillBox">
-                    <span className="styles.title">HTML</span>
-                    <div className="styles.skillBar">
-                        <span className="styles.skillPer html">
-                            <span className="styles.tooltip">90%</span>
-                        </span>
-                    </div>
-                </div>
-            </div>
+    <div className="styles.skillContainer">
+      <div className={styles.titleText}>Skill Lavel</div>
+      {/* html skill */}
+      <div className={styles.skill}>
+        <div className={styles.skillBox}>
+          <span className={styles.title}>HTML</span>
+          <div className={styles.skillBar}>
+            <span className={`${styles.skillPer} ${styles.html}`}>
+              <span className={styles.tooltip}>90%</span>
+            </span>
+          </div>
         </div>
+      </div>
+    </div>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
+    <>
       <h2 className="text-3xl">Home Page</h2>
       <SkillBar/>
-    </main>
+    </>
   );
 }
